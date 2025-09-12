5a3de2166c138278ee7127639a2fe566140d6dff
Alena Petrych <abelokonskaya.87@gmail.com>
2025-09-12 09:38:07 +0100

    feat: added script for custom player

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+/**
+ * Script for navbar (smooth appearence burger-menu)
+ */
 const navbarToggler = document.querySelector(".navbar-toggler");
 const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
 
@@ -7,3 +10,37 @@ navbarToggler.addEventListener("click", () => {
     link.style.opacity = link.style.opacity === "1" ? "0" : "1";
   });
 });
+
+/**
+ * Script for player
+ */
+
+const audio = document.getElementById("audio");
+const playPauseBtn = document.getElementById("play-pause");
+const progress = document.getElementById("progess");
+const timeDisplay = document.getElementById("time");
+
+playPauseBtn.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.play();
+    playPauseBtn.textContent = "⏸";
+  } else {
+    audio.paused();
+    playPauseBtn.textContent = "▶";
+  }
+});
+
+audio.addEventListener("timeupdate", () => {
+  progress.value = (audio.currentTime / audio.duration) * 100;
+  timeDisplay.textContent = formatTime(audio.currentTime);
+});
+
+progress.addEventListener("input", () => {
+  audio.currentTime = (progress.value / 100) * audio.duration;
+});
+
+function formatTime(seconds) {
+  const min = Math.floor(seconds / 60);
+  const sec = Math.floor(seconds % 60);
+  return `${min}:${sec < 10 ? "0" + sec : sec}`;
+}
